bbba39aefc53112db4c967aa7875f26c0c347cae
Daniel Majirsky <daniel@unimus.net>
2019-09-26 12:22:40 +0200

    #29 Vaadin version increased to 14.0.6. Login loading bug fixed.

--- src/main/java/software/netcore/radman/Radman.java
+++ src/main/java/software/netcore/radman/Radman.java
@@ -2,12 +2,15 @@ package software.netcore.radman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
+ * @see <a href="https://vaadin.com/forum/thread/17784869/vaadin-14-with-spring-security-login-page-not-loading">
+ * why ErrorMvcAutoConfiguration.class is excluded</a>
  * @since v. 1.0.0
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class Radman extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
